720b68dbfeab55c01f9a27b690e3e40626766202
Natalie Frick <54511498+nataliefrick@users.noreply.github.com>
2022-10-17 11:55:53 +0200

    retrieved deleted files

---
+++ src/views/deleteUpdateView.vue
@@ -0,0 +1,16 @@
+<template>
+    <Form @plantAdded="getPlants()"/>
+</template>
+
+<script>
+import Form from "../components/Form.vue";
+export default {
+    components: {
+        Form
+    },
+    emits: ["addPlant"],
+    mounted() {
+        this.getPlants();
+    }
+}
+</script>
