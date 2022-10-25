7f096098dd53402519051ddcb0ac974295b5b47c
Natalie Frick <54511498+nataliefrick@users.noreply.github.com>
2022-10-25 12:43:07 +0200

    table2

---
+++ src/components/Tool.vue
@@ -0,0 +1,25 @@
+<template>
+    <li><span class="italics green-text">{{tool.title}}: </span><span class="bold"> {{tool.content}}</span></li>
+</template>
+
+<script>
+export default {
+    props: {
+        tool: Object
+    }
+}
+</script>
+
+<style scoped>
+span.italics {
+    font-style: italic;
+} 
+
+span.bold {
+    font-weight: bold;
+}
+
+li {
+    margin: 0.6em 0;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,88 @@
 <template>
-    <h1>About</h1>
+    <div class="container">
+        <div class="section col s12">
+            <div class="card padding white nohover">
+                <span class="card-title green-text">Website tools</span>
+                <p>This website was built on the following tools.</p>
+                <ul>
+                    <Tool v-for="tool in tools" :tool="tool" :key="tool.id" />
+                </ul>
+            </div>
+        </div>
+    </div>
+
 </template>
+
+<style scoped>
+div.card.padding {
+    padding: 2em;
+}
+
+/* span.italics {
+    font-style: italic;
+}
+
+span.bold {
+    font-weight: bold;
+} */
+
+ul {
+    padding-left: 2em!important;
+}
+</style>
+
+<script>
+import Tool from "../components/Tool.vue";
+export default {
+    data() {
+        return {
+            tools: [],
+            errorMessage : null,
+            urlGet: "https://arcane-hamlet-64136.herokuapp.com/api/about"
+        }
+    },
+    components: {
+        Tool
+    },
+    methods: {
+        async getToolList() {
+            // console.log("getPlants" + urlGet);
+            const response = await fetch("https://arcane-hamlet-64136.herokuapp.com/api/about", {
+                method: "GET",
+                headers: {
+                    "Accept" :  "application/json",
+                    "Content-type" : "application/json",
+                }
+            });
+
+            const data = await response.json(); // save the data in sent through the response.
+
+            this.tools = data;
+        }
+    },
+    mounted() {
+        this.getToolList();
+    }
+}
+</script>
+
+
+<!-- 
+
+"title" : "Frontend deployment",
+"content" : "Netifly"
+
+"title" : "Frontend frameword",
+"content" : "Vue.js"
+
+"title" : "Frontend styling framework",
+"content" : "MaterializeCSS"
+
+"title" : "Backend deployment",
+"content" : "Heruko"
+
+"title" : "Backend framework",
+"content" : "Laravel"
+
+ -->
+
